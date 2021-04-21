0d41bab00491e29ce5da6a995b0fe1b1754ff16e
LAPTOP-34L1HQE4\admin <phongbmt178@gmail.com>
2021-04-21 21:32:36 +0700

    Homework_1

--- app.js
+++ app.js
@@ -1,22 +1,41 @@
-const navItems = [{
+const navItem = [{
         name: "Item 1",
+        chidlren: ['Item 1.1', 'Item 1.2', 'Item 1.3'],
     },
     {
         name: "Item 2",
+        chidlren: [
+            'Item 1.1',
+            'Item 1.2',
+            'Item 1.3',
+            'Item 1.1',
+            'Item 1.2',
+            'Item 1.3',
+        ],
     },
     {
-        name: "Item 3",
+        name: "Item 3"
     },
     {
-        name: "Item 4",
+        name: "Item 4"
     },
     {
-        name: "Item 5",
+        name: "Item 5"
     },
 ];
 
 window.onload = function() {
-    for (let a of navItems) {
-        document.getElementsByClassName("nav_")[0].innerHTML += `<span>` + a.name + `</span>`;
-    }
+    const nav = document.querySelector('.nav_123');
+    navItem.forEach((el) => {
+
+        if (el.chidlren !== undefined) {
+            nav.insertAdjacentHTML('beforeend', `<span class="dropdown">${el.name}</span>`);
+            nav.lastChild.insertAdjacentHTML('beforeend', `<div class="dropdown-content"></div>`);
+            el.chidlren.forEach((child) => {
+                nav.lastChild.lastChild.insertAdjacentHTML('beforeend', `<span>${child}</span>`)
+            })
+        } else {
+            nav.insertAdjacentHTML('beforeend', `<span>${el.name}</span>`);
+        }
+    });
 }
